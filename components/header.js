bc1144e1414acb71bb919c8aece49b392d65a6ea
Tuncay KAYA <tuncay.kaya@powercoders.org>
2024-11-15 19:59:52 +0100

    Added some new nav info

--- components/header.js
+++ components/header.js
@@ -5,8 +5,14 @@ const Header = () => {
     return (
         <header className='fixed top-0 w-full'>
             <LogoImage/>
-
-            This is header with vlad
+            <div class="logo">MyLogo</div>
+            <nav>
+            <ul>
+                <li><a href="index.html">Main</a></li>
+                <li><a href="terms.html">Terms</a></li>
+                <li><a href="about.html">Namnam</a></li>
+            </ul>
+            </nav>
         </header>
     );
 };
