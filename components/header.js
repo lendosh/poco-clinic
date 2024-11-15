0d5d6fa0b121d62bc6cb6fdd9f2dd8d9c89e65fd
TKonstantinova <tetiana.konstantinova@powercoders.org>
2024-11-15 15:44:10 +0100

    Merge main into SCRUM-3

---
+++ components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LogoImage from './logo';
+
+const Header = () => {
+    return (
+        <header className='fixed top-0 w-full'>
+            <LogoImage/>
+
+            This is header with vlad
+        </header>
+    );
+};
+
+export default Header;
