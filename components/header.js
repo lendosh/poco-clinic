eeb7bc2dae2f029d785bf77b47bdac8aed9d6d40
Tuncay KAYA <tuncay.kaya@powercoders.org>
2024-11-18 12:33:18 +0100

    Designed a good shaped header at the end..

--- components/header.js
+++ components/header.js
@@ -3,14 +3,28 @@ import LogoImage from './logo';
 
 const Header = () => {
     return (
-        <header className='fixed top-0 w-full'>
-            <LogoImage/>
-            <div class="topnav">
-                <a class="active" href="#home">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-            </div>
+        <header className='flex justify-start items-center px-4 py-2 bg-inherit shadow-md top-0 w-full gap-x-8'>
+            <LogoImage />
+            <nav className="flex justify-start space-x-4">
+                <a
+                    href="/"
+                    className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+                >
+                    Home
+                </a>
+                <a
+                    href="/booking"
+                    className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+                >
+                    Booking
+                </a>
+                <a
+                    href="/about"
+                    className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+                >
+                    About
+                </a>
+            </nav>
         </header>
     );
 };
--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Image from 'next/image';
 
 const LogoImage = () => {
     return (
         <div>
-            logologo
+            <Image  className="rounded-full flex items-center space-x-2"  
+                    src="/hospital-logo.jpg" 
+                    width={150} 
+                    height={150}
+                    alt="Logo"/>
         </div>
     );
 };
