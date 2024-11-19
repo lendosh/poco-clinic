3ead8eb58da5b49eceb0bd998650eeb27a9ce0d2
lendosh <vladyslav.pomazanov@gmail.com>
2024-11-19 13:33:59 +0100

    change logo img, change logo img type svg, add px to layout

--- components/header.js
+++ components/header.js
@@ -4,8 +4,21 @@ import NavigationLinks from './navigation-links';
 
 const Header = () => {
     return (
-        <header className='fixed flex justify-between items-center px-4 py-2 bg-inherit shadow-md top-0 w-full gap-x-8'>
-            <LogoImage />
+        <header
+            className='
+            fixed
+            flex
+            justify-between
+            items-center
+            py-2
+            px-32
+            bg-inherit
+            shadow-md
+            top-0
+            w-full
+            gap-x-8
+        '>
+            <LogoImage/>
             <NavigationLinks orientation='horizontal'/>
         </header>
     );
