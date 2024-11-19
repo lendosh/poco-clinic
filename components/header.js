73ef37c5a07e5198cc74b76dab8df4ed8a6b3150
Tuncay KAYA <tuncay.kaya@powercoders.org>
2024-11-19 12:51:25 +0100

    Added login div

--- components/header.js
+++ components/header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LogoImage from './logo';
 import NavigationLinks from './navigation-links';
+import LoginArea from './login';
 
 const Header = () => {
     return (
         <header className='fixed flex justify-between items-center px-4 py-2 bg-inherit shadow-md top-0 w-full gap-x-8'>
             <LogoImage />
             <NavigationLinks orientation='horizontal'/>
+            <LoginArea />
         </header>
     );
 };
