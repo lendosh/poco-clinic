70131bb8bbe073085c9b97c4aa6187b8e22e1de1
TKonstantinova <tetiana.konstantinova@powercoders.org>
2024-11-21 14:58:02 +0100

    Main div className update for footer to  stick to bottom

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="
-                    flex-1 
-                    justify-center 
-                    items-center">
+                  flex-1 
+                  justify-center 
+                  items-center">
       Home page Lorem Ipsum Home page Lorem Ipsum 
       Home page Lorem Ipsum Home page Lorem Ipsum 
       Home page Lorem Ipsum Home page Lorem Ipsum 
