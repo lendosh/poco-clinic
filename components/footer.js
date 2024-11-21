7df00479d53d5aff350053b2b5953a38ef607738
TKonstantinova <tetiana.konstantinova@powercoders.org>
2024-11-21 12:40:12 +0100

    Unfix footer, add top shadem, change contacts emoji to icons

--- components/footer.js
+++ components/footer.js
@@ -6,12 +6,16 @@ import SocialLinks from '@/components/social-links';
 const Footer = () => {
     return (
         <footer className='
-            bg-slate-100 
-            text-gray-800 
-            flex space-x-2 
-            fixed bottom-0 w-full 
-            py-4 px-32 text-left
-            justify-between items-center'
+            flex
+            bg-inherit
+            text-swans-down-800 
+            space-x-2 
+            py-4
+            px-32
+            justify-between 
+            items-center
+            w-full
+            [box-shadow:0_-4px_6px_-1px_rgba(0,0,0,0.1),0_-2px_4px_-1px_rgba(0,0,0,0.06)]'
         >
             <section className='flex gap-x-32'>
                 <Contacts />
