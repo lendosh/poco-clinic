60171532542f4355b328528d1cb7b6a28bd23c1c
lendosh <vladyslav.pomazanov@gmail.com>
2024-11-21 22:34:14 +0100

    update contacts.js

--- components/contacts.js
+++ components/contacts.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import {MailIcon, Phone, MapPin, Clock} from "lucide-react";
 
-
 const Contacts = () => {
     return (
         <section className='flex-col align-bottom space-y-1.5 p-1 w-full'>
-            <p className='text-xl font-semibold pb-2'><span className='text-red-700'>&#123;</span>POWER<span className='text-red-700'>.</span>CODERS<span className='text-red-700'>&#125;</span></p>
-            <ul>
-            
-                <li><MapPin className='h-5 w-5 mr-2 text-swans-down-700 inline'></MapPin><a href="https://maps.app.goo.gl/V2pDiHLdXp1iGBiDA" target="_blank">Marktgasse 46, 3011 Bern</a></li>
-                <li><MailIcon className='h-5 w-5 mr-2 text-swans-down-700 inline'></MailIcon><a href="mailto:info@powercoders.org">info@powercoders.org</a></li>
-                <li><Phone className='h-5 w-5 mr-2 text-swans-down-700 inline'></Phone><a href="tel:+41791234567">+41 (79) 123-45-67</a></li>
-                <li><Clock className='h-5 w-5 mr-2 text-swans-down-700 inline'></Clock>Monday to Friday, 8 am to 6 pm</li>
+            <p className='text-xl flex items-center font-semibold pb-2'>
+                <span className='text-red-700'>
+                    &#123;
+                </span>
+                <span className='flex items-center pt-1'>
+                    POWER
+                    <span className='text-red-700'>.</span>
+                    CODERS
+                </span>
+                <span className='text-red-700'>&#125;</span>
+            </p>
+            <ul className='flex flex-col gap-y-1'>
+                <li className='flex gap-x-2 items-center justify-start text-center'>
+                    <MapPin className='h-5 w-5 mr-2 text-swans-down-700 inline'/>
+                    <a href="https://maps.app.goo.gl/V2pDiHLdXp1iGBiDA" target="_blank" className='font-bold'>Marktgasse 46, 3011 Bern</a>
+                </li>
+                <li className='flex gap-x-2 items-center justify-start text-center'>
+                    <MailIcon className='h-5 w-5 mr-2 text-swans-down-700 inline'/>
+                    <a href="mailto:info@powercoders.org" className='font-bold'>info@powercoders.org</a>
+                </li>
+                <li className='flex gap-x-2 items-center justify-start text-center'>
+                    <Phone className='h-5 w-5 mr-2 text-swans-down-700 inline'/>
+                    <a href="tel:+41791234567" className='font-bold'>+41 (79) 123-45-67</a>
+                </li>
+                <li className='flex gap-x-2 items-center justify-start text-center'>
+                    <Clock className='h-5 w-5 mr-2 text-swans-down-700 inline'/>
+                    <span className='font-bold'>Monday to Friday, 8 am to 6 pm</span>
+                </li>
             </ul>
         </section>
     );
