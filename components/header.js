808282b396164c92e2bc2cda269c0c24189f249a
lendosh <vladyslav.pomazanov@gmail.com>
2024-11-24 23:27:47 +0100

    add emergency header

--- components/header.js
+++ components/header.js
@@ -5,9 +5,14 @@ import LoginArea from './login';
 
 const Header = () => {
     return (
-        <header
-            className='
-            fixed
+        <header className='flex flex-col w-full fixed top-0'>
+            <section className='bg-slate-300 text-rose-500 w-full text-center flex gap-x-2 justify-center'>
+                <a href="https://maps.app.goo.gl/V2pDiHLdXp1iGBiDA" target="_blank" className='font-bold'>Marktgasse 46, 3011 Bern,</a>
+                <a href="tel:+41791234567" className='font-bold text-rose-500'>TEL: +41 (79) 123-45-67</a>
+            </section>
+            <section
+                className='
+
             flex
             justify-between
             items-center
@@ -17,11 +22,12 @@ const Header = () => {
             shadow-md
             top-0
             w-full
-            gap-x-8
-        '>
-            <LogoImage/>
-            <NavigationLinks orientation='horizontal'/>
-            <LoginArea />
+            gap-x-8'
+            >
+                <LogoImage/>
+                <NavigationLinks orientation='horizontal'/>
+                <LoginArea />
+            </section>
         </header>
     );
 };
