f10d296656a761fc791ba169c59ecbdfb1236dbe
TKonstantinova <tetiana.konstantinova@powercoders.org>
2024-11-25 23:08:22 +0100

    Update padding between header & main emergency page text styles

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -3,12 +3,13 @@ import ContactCardsList from '@/components/contact-us-cards-list';
 
 const ContactUsPage = () => {
     return (
-        <main className='flex flex-col w-full gap-y-2 items-center pt-12'>
+        <main className='flex flex-col w-full gap-y-2 items-center'>
             <h1 className='
                             text-4xl
                             text-center
                             text-swans-down-950
                             font-bold
+                            pb-6
                             '>
                 Contact Us
             </h1>
