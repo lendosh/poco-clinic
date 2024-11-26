1588b35c711c1a9de4e15d5247ae5bd9d0f035a6
lendosh <vladyslav.pomazanov@gmail.com>
2024-11-26 22:33:08 +0100

    fix termin page

--- app/booking/page.js
+++ app/booking/page.js
@@ -9,27 +9,21 @@ const TerminPage = async () => {
 
     return (
         <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Book Your Appointment</h1>
-
-
-           
+            <h1 className="text-2xl font-bold">Book Your Appointment</h1>
             <div className="flex flex-col md:flex-row justify-center items-center gap-8">
                 {/* Info Text */}
                 <div className="flex-1 text-center md:text-left">
-               
                     <p className="mb-4">We are here to assist you! Whether you are seeking medical advice, have questions about our services, or need assistance with booking an appointment, our dedicated team is here to ensure you have the best experience at POCO-Clinic.</p>
-
-                    <br></br>
-
                     <p className="mb-4">Our professionals are committed to providing personalized care, so please allow up to 24 hours during business hours for a response. Our business hours are Monday to Friday, from 8:00 AM to 6:00 PM.</p>
                 </div>
-
-                
                 {/* Form */}
                 <div className="flex-1 max-w-lg w-full">
                     <div className="h-[400px] overflow-y-auto">
                         {/* Added a fixed height and scrollable container for the form */}
                         <TerminForm doctors={doctors} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
